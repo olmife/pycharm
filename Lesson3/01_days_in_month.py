318e8db307409e04ba58b48410410defe8883384
Олег <olmife@mail.ru>
2022-03-13 21:15:47 +0300

    lesson 3 first

--- Lesson3/01_days_in_month.py
+++ Lesson3/01_days_in_month.py
@@ -10,5 +10,36 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
+if month==1:
+    days= 31
+elif month==2:
+    days=28
+elif month==3:
+    days= 30
+elif month==4:
+    days=31
+elif month==5:
+    days=31
+elif month==6:
+    days= 30
+elif month==7:
+    days = 31
+elif month==8:
+    days = 31
+elif month==9:
+    days = 30
+else :
+    print('Вы ввели некорректное число месяца')
+print('В этом месяце', days, 'дней')
 
-# TODO здесь ваш код
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+print('Вы ввели', month)
+month_list = {
+    1 : 30,
+    2 : 28,
+    3 : 31,
+}
+if month in month_list:K
+    print(month_list.get(month))
+
--- Lesson3/lecture_snippets/proeb.py
+++ Lesson3/lecture_snippets/proeb.py
@@ -1,6 +1,20 @@
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+month_list = {
+    1 : 30,
+    2 : 28,
+    3 : 31,
+}
+if user_input in month_list:
+    print(month_list.get(user_input))
 
-print('дратути!')
-x = 34
-y = 43
-print(x * y)
-print('дотвидания!')
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+print('Вы ввели', month)
+month_list = {
+    1: 30,
+    2: 28,
+    3: 31,
+}
+if month in month_list:
+    print(month_list.get(month))
